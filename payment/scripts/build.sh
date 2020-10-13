ed47571db731e843652939c7da83922587402ed8
awraad <awraadaljably@gmail.com>
2020-10-13 05:30:07 -0700

    edit dockerfile

--- payment/scripts/build.sh
+++ payment/scripts/build.sh
@@ -4,21 +4,9 @@ set -ev
 
 SCRIPT_DIR=$(dirname "$0")
 
-if [[ -z "$GROUP" ]] ; then
-    echo "Cannot find GROUP env var"
-    exit 1
-fi
 
-if [[ -z "$COMMIT" ]] ; then
-    echo "Cannot find COMMIT env var"
-    exit 1
-fi
+DOCKER_CMD=docker
 
-if [[ "$(uname)" == "Darwin" ]]; then
-    DOCKER_CMD=docker
-else
-    DOCKER_CMD="sudo docker"
-fi
 CODE_DIR=$(cd $SCRIPT_DIR/..; pwd)
 echo $CODE_DIR
 
