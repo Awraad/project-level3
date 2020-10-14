6570a86da54e5161dcc514a167f1b4a92432b1a4
awraad <awraadaljably@gmail.com>
2020-10-14 03:43:57 -0700

    s

--- s.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-git clone https://github.com/Awraad/k8s-sandbox
-cd k8s-sandbox
-make up
-
-kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
